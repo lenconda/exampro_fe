39bee67a05ad4cccfeda8a671b67799ec0d44549
lenconda <i@lenconda.top>
2021-03-25 19:22:43 +0800

    feat: register and forget_password

--- src/pages/User/Auth/index.tsx
+++ src/pages/User/Auth/index.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Button, Card, Checkbox, CssBaseline, FormControlLabel, LinearProgress, Typography } from '@material-ui/core';
 import { Field, Form as FormikForm, Formik } from 'formik';
 import { TextField } from 'formik-material-ui';
-import { Fingerprint } from 'mdi-material-ui';
+import { DatabaseCheck, Fingerprint, HeadCheck } from 'mdi-material-ui';
 import { connect } from 'react-redux';
 import { ConnectState } from '../../../models';
 import { AppState } from '../../../models/app';
@@ -17,7 +17,7 @@ const Form = FormikForm as any;
 export interface AuthPageProps extends AppState {}
 
 const AuthPage: React.FC<AuthPageProps> = (props) => {
-  const [authStatus, setAuthStatus] = useState<'login' | 'register'>(null);
+  const [authStatus, setAuthStatus] = useState<'login' | 'register' | 'forget_password'>(null);
   const texts = props.i18n[props.locale].ui['/user/auth'];
   const history = useHistory();
 
@@ -27,105 +27,137 @@ const AuthPage: React.FC<AuthPageProps> = (props) => {
       <Card classes={{ root: 'app-page-auth__card' }} elevation={0}>
         <div className="app-page-auth__card__icons">
           <CssBaseline />
-          <Fingerprint fontSize="large" className="icon" />
-          <Typography className="title">{texts['001']}</Typography>
+          {
+            (!authStatus || authStatus === 'login') && (
+              <Fingerprint fontSize="large" className="icon" />
+            )
+          }
+          {
+            authStatus === 'register' && (
+              <DatabaseCheck fontSize="large" className="icon success" />
+            )
+          }
+          {
+            authStatus === 'forget_password' && (
+              <HeadCheck fontSize="large" className="icon success" />
+            )
+          }
+          <Typography className="title">
+            {
+              (!authStatus || authStatus === 'login') && texts['001']
+            }
+            {
+              authStatus === 'register' && texts['011']
+            }
+            {
+              authStatus === 'forget_password' && texts['012']
+            }
+          </Typography>
         </div>
-        <Formik
-          initialValues={{
-            email: '',
-            password: '',
-          }}
-          validate={(values) => {
-            const errors = {} as Record<string, any>;
-            if (!values.email) {
-              errors.email = texts['009'];
-            } else if (
-              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-            ) {
-              errors.email = texts['008'];
-            }
-            return errors;
-          }}
-          onSubmit={(values, { setSubmitting }) => {
-            const { email, password } = values;
-            if (!authStatus) {
-              getEmailAuthType(email)
-                .then((type) => {
-                  setAuthStatus(type);
-                })
-                .finally(() => setSubmitting(false));
-            }
-            if (authStatus === 'login') {
-              login(email, password)
-                .then((res) => {
-                  if (res) {
-                    const location = (_.get(history, 'location.search') || '').slice(1);
-                    if (location.redirect) {
-                      history.push(Base64.decode(location.redirect));
-                    } else {
-                      history.push('/');
-                    }
-                  }
-                })
-                .finally(() => setSubmitting(false));
-            }
-          }}
-        >
-          {({ submitForm, isSubmitting }) => (
-            <Form className="app-form app-page-auth__card__form">
-              <Field
-                component={TextField}
-                name="email"
-                type="email"
-                label={texts['002']}
-                variant="outlined"
-                className="input-wrapper"
-                InputProps={{
-                  className: 'input',
-                  disabled: authStatus === 'login',
+        {
+          (!authStatus || authStatus === 'login')
+            ? (
+              <Formik
+                initialValues={{
+                  email: '',
+                  password: '',
                 }}
-              />
-              {
-                authStatus === 'login' && (
-                  <>
+                validate={(values) => {
+                  const errors = {} as Record<string, any>;
+                  if (!values.email) {
+                    errors.email = texts['009'];
+                  } else if (
+                    !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+                  ) {
+                    errors.email = texts['008'];
+                  }
+                  return errors;
+                }}
+                onSubmit={(values, { setSubmitting }) => {
+                  const { email, password } = values;
+                  if (!authStatus) {
+                    getEmailAuthType(email)
+                      .then((type) => {
+                        setAuthStatus(type);
+                      })
+                      .finally(() => setSubmitting(false));
+                  }
+                  if (authStatus === 'login') {
+                    login(email, password)
+                      .then((res) => {
+                        if (res) {
+                          const location = (_.get(history, 'location.search') || '').slice(1);
+                          if (location.redirect) {
+                            history.push(Base64.decode(location.redirect));
+                          } else {
+                            history.push('/');
+                          }
+                        }
+                      })
+                      .finally(() => setSubmitting(false));
+                  }
+                }}
+              >
+                {({ submitForm, isSubmitting }) => (
+                  <Form className="app-form app-page-auth__card__form">
                     <Field
                       component={TextField}
-                      type="password"
-                      label={texts['003']}
-                      name="password"
+                      name="email"
+                      type="email"
+                      label={texts['002']}
                       variant="outlined"
                       className="input-wrapper"
                       InputProps={{
                         className: 'input',
+                        disabled: authStatus === 'login',
                       }}
                     />
-                    <FormControlLabel
-                      classes={{ root: 'checkbox-wrapper' }}
-                      label={texts['006']}
-                      control={
-                        <Checkbox
-                          color="primary"
-                          defaultChecked={JSON.parse(localStorage.getItem('persist') || 'false')}
-                          onChange={(event) => {
-                            localStorage.setItem('persist', JSON.stringify(event.target.checked));
-                          }}
-                        />
-                      }
-                    />
-                  </>
-                )
-              }
-              <Button
-                variant="contained"
-                color="primary"
-                className="button-wrapper"
-                disabled={isSubmitting}
-                onClick={submitForm}
-              >{authStatus === 'login' ? texts['010'] : texts['004']}</Button>
-              {isSubmitting && <LinearProgress />}
-            </Form>
-          )}
-        </Formik>
+                    {
+                      authStatus === 'login' && (
+                        <>
+                          <Field
+                            component={TextField}
+                            type="password"
+                            label={texts['003']}
+                            name="password"
+                            variant="outlined"
+                            className="input-wrapper"
+                            InputProps={{
+                              className: 'input',
+                            }}
+                          />
+                          <FormControlLabel
+                            classes={{ root: 'checkbox-wrapper' }}
+                            label={texts['006']}
+                            control={
+                              <Checkbox
+                                color="primary"
+                                defaultChecked={JSON.parse(localStorage.getItem('persist') || 'false')}
+                                onChange={(event) => {
+                                  localStorage.setItem('persist', JSON.stringify(event.target.checked));
+                                }}
+                              />
+                            }
+                          />
+                        </>
+                      )
+                    }
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      className="button-wrapper"
+                      disabled={isSubmitting}
+                      onClick={submitForm}
+                    >{authStatus === 'login' ? texts['010'] : texts['004']}</Button>
+                    {isSubmitting && <LinearProgress />}
+                  </Form>
+                )}
+              </Formik>
+            )
+            : <>
+              <Typography variant="subtitle1" style={{ textAlign: 'center' }}>{texts['013']}</Typography>
+            </>
+        }
       </Card>
     </div>
   );
